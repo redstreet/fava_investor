1ba79d7134bd4d07e020a998112c640d95b89e15
Red S <redstreet@users.noreply.github.com>
2020-03-31 01:31:50 -0700

    create modules directory

--- __init__.py
+++ __init__.py
@@ -3,8 +3,8 @@
 
 from fava.ext import FavaExtensionBase
 
-from tlh import libtlh
-from aa_byaccount import libaaacc
+from modules.tlh import libtlh
+from modules.aa_byaccount import libaaacc
 
 # -----------------------------------------------------------------------------------------------------------
 class TaxLossHarvester(FavaExtensionBase):  # pragma: no cover
