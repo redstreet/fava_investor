ee4dff092102068327cece3e3e4c4b42ce344ce7
Red S <redstreet@users.noreply.github.com>
2020-04-10 02:08:59 -0700

    api updates

--- fava_investor/common/beancountinvestorapi.py
+++ fava_investor/common/beancountinvestorapi.py
@@ -5,11 +5,13 @@ from beancount.core import getters
 from beancount.core import prices
 from beancount.core import realization
 from beancount.query import query
+from beancount.core.data import Open
 
 class AccAPI:
     def __init__(self, beancount_file, options):
         self.entries, _, self.options_map = loader.load_file(beancount_file)
         self.options = options
+        self.begin = self.end = None # Only used in fava
 
     def build_price_map(self):
         return prices.build_price_map(self.entries)
@@ -20,6 +22,14 @@ class AccAPI:
     def realize(self):
         return realization.realize(self.entries)
 
+    def root_tree(self):
+        from fava.core import Tree
+        return Tree(self.entries)
+
+        # rrr = realization.realize(self.entries)
+        # import pdb; pdb.set_trace()
+        # return realization.realize(self.entries)
+
     def query_func(self, sql):
         rtypes, rrows = query.run_query(self.entries, self.options_map, sql)
         return rtypes, rrows
@@ -29,3 +39,15 @@ class AccAPI:
 
     def get_account_open_close(self):
         return getters.get_account_open_close(self.entries)
+
+    def get_account_open(self):
+        oc = getters.get_account_open_close(self.entries)
+        opens = [e for e in oc if isintance(e, Open)]
+        return opens
+
+    # def cost_or_value(self, node, date, include_children):
+    #     invent inventory.reduce(get_market_value, g.ledger.price_map, date)
+    #     if include_children:
+    #         return cost_or_value(node.balance_children, date)
+    #     return cost_or_value(node.balance, date)
+
--- fava_investor/common/favainvestorapi.py
+++ fava_investor/common/favainvestorapi.py
@@ -1,7 +1,13 @@
 from beancount.core import getters
+from fava.template_filters import cost_or_value
+
 class FavaInvestorAPI:
-    def __init__(self, ledger):
+    def __init__(self, ledger, begin=None, end=None):
+        # TODO: are begin/end needed?
         self.ledger = ledger
+        self.begin = begin
+        self.end = end
+        self.entries = self.ledger.root_tree
 
     def build_price_map(self):
         return self.ledger.price_map
@@ -12,6 +18,9 @@ class FavaInvestorAPI:
     def realize(self):
         return self.ledger.root_account
 
+    def root_tree(self):
+        return self.ledger.root_tree
+
     def query_func(self, sql):
         contents, rtypes, rrows = self.ledger.query_shell.execute_query(sql)
         return rtypes, rrows
@@ -21,4 +30,9 @@ class FavaInvestorAPI:
 
     def get_account_open_close(self):
         return getters.get_account_open_close(self.ledger.entries)
+
+    def cost_or_value(self, node, date, include_children):
+        if include_children:
+            return cost_or_value(node.balance_children, date)
+        return cost_or_value(node.balance, date)
 
