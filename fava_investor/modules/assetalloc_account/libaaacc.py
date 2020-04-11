f69cca912eb61c9e2df5612ff7e9b1991c1a2ef4
Red S <redstreet@users.noreply.github.com>
2020-04-10 18:09:13 -0700

    refactor libaaacc to use accapi

--- fava_investor/modules/assetalloc_account/libaaacc.py
+++ fava_investor/modules/assetalloc_account/libaaacc.py
@@ -5,23 +5,22 @@ import re
 from beancount.core.data import iter_entry_dates, Open
 from beancount.core.number import ZERO, Decimal
 
-from fava.template_filters import cost_or_value
-
-def portfolio_accounts(tree, configs, ledger, end):
+def portfolio_accounts(accapi, configs):
     """An account tree based on matching regex patterns."""
 
     portfolios = []
     for config in configs:
         pattern_type = config['pattern_type']
         func = globals()['by_' + pattern_type]
-        portfolio = func(tree, end, config, ledger)
+        portfolio = func(accapi, config)
         portfolios.append(portfolio)
 
     return portfolios
 
-def by_account_name(tree, date, config, ledger):
+def by_account_name(accapi, config):
     """Returns portfolio info based on matching account name."""
 
+    tree = accapi.root_tree()
     pattern = config['pattern']
     include_children = config.get('include_children', False)
     title = config.get('title', "Account names matching: '" + pattern + "'")
@@ -33,10 +32,10 @@ def by_account_name(tree, date, config, ledger):
             selected_accounts.append(acct)
 
     selected_nodes = [tree[x] for x in selected_accounts]
-    portfolio_data = asset_allocation(selected_nodes, date, ledger, include_children)
+    portfolio_data = asset_allocation(selected_nodes, accapi, include_children)
     return title, portfolio_data
 
-def by_account_open_metadata(tree, date, config, ledger):
+def by_account_open_metadata(accapi, config):
     """ Returns portfolio info based on matching account open metadata. """
 
     metadata_key = config['metadata_key']
@@ -45,18 +44,19 @@ def by_account_open_metadata(tree, date, config, ledger):
 
     selected_accounts = []
     regexer = re.compile(pattern)
-    for entry in ledger.all_entries_by_type[Open]:
+    for entry in accapi.all_entries_by_type[Open]:
         if metadata_key in entry.meta and regexer.match(entry.meta[metadata_key]) is not None:
             selected_accounts.append(entry.account)
 
-    selected_nodes = [tree[x] for x in selected_accounts]
-    portfolio_data = asset_allocation(selected_nodes, date, ledger, include_children)
+    selected_nodes = [accapi.root_tree()[x] for x in selected_accounts]
+    portfolio_data = asset_allocation(selected_nodes, accapi, include_children)
     return title, portfolio_data
 
-def asset_allocation(nodes, date, ledger, include_children):
+def asset_allocation(nodes, accapi, include_children):
     """Compute percentage of assets in each of the given nodes."""
 
-    operating_currency = ledger.options["operating_currency"][0]
+    date = accapi.end
+    operating_currency = accapi.get_operating_currency()
     acct_type = ("account", str(str))
     bal_type = ("balance", str(Decimal))
     alloc_type = ("allocation %", str(Decimal))
@@ -64,9 +64,8 @@ def asset_allocation(nodes, date, ledger, include_children):
 
     rows = []
     for node in nodes:
-        row = {}
-        row["account"] = node.name
-        balance = cost_or_value(node.balance_children, date) if include_children else cost_or_value(node.balance, date)
+        row = {'account': node.name}
+        balance = accapi.cost_or_value(node.balance_children, date, include_children)
         if operating_currency in balance:
             row["balance"] = balance[operating_currency]
             rows.append(row)
