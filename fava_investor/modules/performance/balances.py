9f8806b3d6b1291bda0ac93ae7367c48661b0a72
Red S <redstreet@users.noreply.github.com>
2020-04-21 01:58:04 -0700

    refactored balances.py

--- fava_investor/modules/performance/balances.py
+++ fava_investor/modules/performance/balances.py
@@ -1,71 +1,41 @@
 import re
-
 from beancount.core.account import parent, parents
-from fava.core import FavaLedger, Tree
-from fava.core.tree import TreeNode
 
-
-def get_closed_tree_with_value_accounts_only(accapi, config) -> Tree:
-    ledger: FavaLedger = accapi.ledger
-    tree = ledger.root_tree_closed
-    accounts_to_keep = _get_value_accounts_and_parents(
-        accapi.ledger.accounts, config.get("accounts_patterns", [".*"])
-    )
-    _filter_tree(tree, accounts_to_keep)
+def get_closed_tree_with_value_accounts_only(accapi, config):
+    tree = accapi.ledger.root_tree_closed
+    accounts_to_keep = get_value_accounts_and_parents(tree, accapi.ledger.accounts,
+            config.get("accounts_pattern", "^Assets:.*"))
+    filter_tree(tree, accounts_to_keep)
     return tree
 
+def get_value_accounts_and_parents(tree, accounts, pattern ):
+    result = set([acc for acc in accounts if re.match(pattern, acc)])
+    ancestors = [p.name for acc in result for p in tree.ancestors(acc)]
+    return result.union(ancestors)
 
-def get_value_accounts(accounts, patterns):
-    result = set()
-    for account in accounts:
-        if _is_value_account(account, patterns):
-            result.add(account)
-    return result
+def filter_tree(tree, accounts_to_keep):
+    nodes_to_remove = [n for n in tree if n not in accounts_to_keep]
+    nodes_to_remove.sort(key = lambda s: len(s), reverse = True)
+    for account in nodes_to_remove:
+        remove_account_from_tree(tree, account)
 
-
-def _remove_account_from_tree(tree: Tree, account: str):
-    if account not in tree or account == "":
+def remove_account_from_tree(tree, account):
+    if account not in tree or not account:
         return
+
     node = tree[account]
-    for child in list(node.children):
-        _remove_account_from_tree(tree, child.name)
-
-    _remove_from_parent(account, node, tree)
-    _reduce_parents_balances(account, node, tree)
-
+    remove_from_parent(account, node, tree)
+    reduce_parents_balances(account, node, tree)
     del tree[account]
 
-
-def _get_value_accounts_and_parents(accounts: dict, patterns):
-    result = get_value_accounts(accounts, patterns)
-
-    for value_acc in list(result):
-        for p in parents(value_acc):
-            result.add(p)
-    return result
-
-
-def _filter_tree(tree, accounts_to_keep):
-    for account in list(tree.keys()):
-        if account not in accounts_to_keep:
-            _remove_account_from_tree(tree, account)
-
-
-def _reduce_parents_balances(account, node, tree):
-    for parent_account in parents(account):
-        parent_node: TreeNode = tree[parent_account]
-        parent_node.balance_children.add_inventory(-node.balance)
-
-
-def _remove_from_parent(account, node, tree):
+def remove_from_parent(account, node, tree):
     parent_account = parent(account)
-    if parent_account is not None:
-        parent_node: TreeNode = tree[parent_account]
+    if parent_account:
+        parent_node = tree[parent_account]
         parent_node.children.remove(node)
 
-
-def _is_value_account(account, patterns):
-    for pattern in patterns:
-        if re.match(pattern, account):
-            return True
-    return False
+def reduce_parents_balances(account, node, tree):
+    for parent_account in parents(account):
+        parent_node = tree[parent_account]
+        parent_node.balance_children.add_inventory(-node.balance)
+        parent_node.balance_children.add_inventory(-node.balance_children)
