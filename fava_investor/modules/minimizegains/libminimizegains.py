f412416ce62d71c45c2c9d538d7d68fdd72c31fa
Red S <redstreet@users.noreply.github.com>
2022-08-28 18:43:43 -0700

    feat(minimizegains): rearrange columns for readability

--- fava_investor/modules/minimizegains/libminimizegains.py
+++ fava_investor/modules/minimizegains/libminimizegains.py
@@ -60,19 +60,22 @@ def find_minimized_gains(accapi, options):
             est_tax = gain * tax_rate[term]
 
             to_sell.append(RetRow(row.account, row.units, row.acquisition_date, row.market_value,
-                gain, term, est_tax, (est_tax/val(row.market_value))*100))
+                gain, term, est_tax, (est_tax / val(row.market_value)) * 100))
 
     to_sell.sort(key=lambda x: x.est_tax_percent)
 
     # add cumulative column
-    retrow_types = retrow_types + [('cumu_proceeds', Decimal), ('cumu_gains', Decimal),
-            ('percent', Decimal)]
+    retrow_types = [('cumu_proceeds', Decimal), ('cumu_taxes', Decimal)] + \
+                       retrow_types + \
+                       [('cumu_gains', Decimal), ('percent', Decimal)]
     RetRow = collections.namedtuple('RetRow', [i[0] for i in retrow_types])
     retval = []
-    cumu_proceeds = cumu_gains = 0
+    cumu_proceeds = cumu_gains = cumu_taxes = 0
     for row in to_sell:
         cumu_gains += row.gain
         cumu_proceeds += val(row.market_value)
-        retval.append(RetRow(*row, cumu_proceeds, cumu_gains, (cumu_gains/cumu_proceeds)*100))
+        cumu_taxes += row.est_tax
+        retval.append(RetRow(cumu_proceeds, cumu_taxes, *row, cumu_gains,
+                      (cumu_gains / cumu_proceeds) * 100))
 
     return retrow_types, retval
