574be6070bf64280d35aca4a3db1e975f8c2a8c0
Red S <redstreet@users.noreply.github.com>
2022-10-15 12:36:57 -0700

    ci: flake

--- fava_investor/modules/minimizegains/libminimizegains.py
+++ fava_investor/modules/minimizegains/libminimizegains.py
@@ -43,7 +43,7 @@ def find_minimized_gains(accapi, options):
 
     # our output table is slightly different from our query table:
     retrow_types = rtypes[:-1] + [('gain', Decimal), ('term', str),
-                       ('est_tax', Decimal), ('est_tax_percent', Decimal)]
+                                  ('est_tax', Decimal), ('est_tax_percent', Decimal)]
 
     # rtypes:
     # [('account', <class 'str'>),
@@ -62,14 +62,14 @@ def find_minimized_gains(accapi, options):
             est_tax = gain * tax_rate[term]
 
             to_sell.append(RetRow(row.account, row.units, row.acquisition_date, row.market_value,
-                gain, term, est_tax, (est_tax / val(row.market_value)) * 100))
+                           gain, term, est_tax, (est_tax / val(row.market_value)) * 100))
 
     to_sell.sort(key=lambda x: x.est_tax_percent)
 
     # add cumulative column
-    retrow_types = [('cumu_proceeds', Decimal), ('cumu_taxes', Decimal)] + \
-                   retrow_types + \
+    retrow_types = [('cumu_proceeds', Decimal), ('cumu_taxes', Decimal)] + retrow_types + \
                    [('cumu_gains', Decimal), ('percent', Decimal)]
+
     RetRow = collections.namedtuple('RetRow', [i[0] for i in retrow_types])
     retval = []
     cumu_proceeds = cumu_gains = cumu_taxes = 0
