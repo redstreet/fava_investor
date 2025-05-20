e2b0caa7969630440380ebf99f81d1027595f04d
Megh Parikh <meghprkh@gmail.com>
2025-05-20 00:17:45 -0400

    flake8

--- fava_investor/common/favainvestorapi.py
+++ fava_investor/common/favainvestorapi.py
@@ -68,7 +68,7 @@ class FavaInvestorAPI:
         # TODO: below is probably fava only, and needs to be made beancount friendly
         return g.ledger.all_entries_by_type.Open
 
-    def cost_or_value(self, node, date, include_children, *, operating_currency = None):
+    def cost_or_value(self, node, date, include_children, *, operating_currency=None):
         nodes = node.balance
         if include_children:
             nodes = node.balance_children
